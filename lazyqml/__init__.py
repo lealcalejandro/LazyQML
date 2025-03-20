187f1b1a22b9c282fa392158bed3dd924cedf474
lealcalejandro <lealcalejandro@uniovi.es>
2025-03-20 11:33:20 +0100

    Added global simulation type variable and unittests

--- lazyqml/__init__.py
+++ lazyqml/__init__.py
@@ -3,3 +3,20 @@
 __author__ = """Diego García Vega, Fernando Álvaro Plou Llorente, Alejandro Leal Castaño"""
 __email__ = "garciavdiego@uniovi.es, ploufernando@uniovi.es, lealcalejandro@uniovi.es"
 __version__ = "0.0.8"
+
+import lazyqml
+
+_simulation = "statevector"
+
+def set_simulation_type(sim):
+    try:
+        assert sim == "statevector" or sim == "tensor"
+
+        lazyqml._simulation = sim
+
+    except Exception as e:
+        raise ValueError(f"Simulation type must be \"statevector\" or \"tensor\". Got \"{sim}\"")
+    
+
+def get_simulation_type():
+    return lazyqml._simulation
--- tests/test_lazyqml.py
+++ tests/test_lazyqml.py
@@ -2,17 +2,35 @@
 
 """Tests for `lazyqml` package."""
 
-
 import unittest
-
-
 
 class TestLazyqml(unittest.TestCase):
     """Tests for `lazyqml` package."""
 
     def test_import(self):
         import lazyqml 
-        print("Imported correctly")
-        
+        # print("Imported correctly")
 
-    
+    def test_simulation_strings(self):
+        # Verify getter/setter of simulation type flag
+        from lazyqml import lazyqml
+
+        sim = "statevector"
+        lazyqml.set_simulation_type(sim)
+        self.assertTrue(lazyqml.get_simulation_type(), "statevector")
+
+        sim = "tensor"
+        lazyqml.set_simulation_type(sim)
+        self.assertTrue(lazyqml.get_simulation_type(), "tensor")
+
+        # Verify that ValueError is raised when number or different string is set
+        sim = 3
+        with self.assertRaises(ValueError):
+            lazyqml.set_simulation_type(sim)
+
+        sim = "tns"
+        with self.assertRaises(ValueError):
+            lazyqml.set_simulation_type(sim)
+
+if __name__ == '__main__':
+    unittest.main()
