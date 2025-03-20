2bb03411da41cf3a9c61d5bfbbfc96e8adcb6ae8
lealcalejandro <lealcalejandro@uniovi.es>
2025-03-20 11:31:04 +0100

    Removed redundant code in QSVM execution

--- lazyqml/Factories/Models/QSVM.py
+++ lazyqml/Factories/Models/QSVM.py
@@ -1,11 +1,7 @@
 from lazyqml.Interfaces.iModel import Model
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, balanced_accuracy_score
 import pennylane as qml
-from time import time
 from lazyqml.Factories.Circuits.fCircuits import CircuitFactory
 from lazyqml.Utils.Utils import printer
 
@@ -38,16 +34,8 @@ class QSVM(Model):
     # Not used at the moment, We might be interested in computing our own kernel.
     def _quantum_kernel(self, X1, X2):
         """Calculate the quantum kernel matrix for SVM."""
-        num_samples_1 = len(X1)
-        num_samples_2 = len(X2)
-        kernel_matrix = np.zeros((num_samples_1, num_samples_2))
 
-        for i in range(num_samples_1):
-            for j in range(num_samples_2):
-                kernel_matrix[i, j] = self.kernel_circ(X1[i], X2[j]).sum()
-
-        #return kernel_matrix
-        return np.array([[self.kernel_circ(x1 , x2)[0] for x2 in X2]for x1 in X1])
+        return np.array([[self.kernel_circ(x1, x2)[0] for x2 in X2]for x1 in X1])
 
     def fit(self, X, y):
         self.X_train = X
