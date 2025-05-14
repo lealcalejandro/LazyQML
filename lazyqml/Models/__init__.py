7587182f515a82de033495a3c19818c654c3408a
lealcalejandro <lealcalejandro@uniovi.es>
2025-05-14 14:34:10 +0200

    QNN model using torch layers

--- lazyqml/Models/__init__.py
+++ lazyqml/Models/__init__.py
@@ -2,5 +2,6 @@ from .QNNTorch import QNNTorch
 from .QNNBag import QNNBag
 from .QSVM import QSVM
 from .QKNN import QKNN
+from .QSequential import HybridSequential
 
-__all__ = ['QNNTorch', 'QNNBag', 'QSVM', 'QKNN']
+__all__ = ['QNNTorch', 'QNNBag', 'QSVM', 'QKNN', 'HybridSequential']
