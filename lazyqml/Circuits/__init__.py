03ee66aea4ef2478409784254f26b16d542fd839
lealcalejandro <lealcalejandro@uniovi.es>
2025-05-20 12:32:30 +0200

    Reorganized ansatzs in Circuits

--- lazyqml/Circuits/__init__.py
+++ lazyqml/Circuits/__init__.py
@@ -1,17 +1,14 @@
+## Legacy
 # Embeddings
-from .RxEmbedding import RxEmbedding
-from .RyEmbedding import RyEmbedding
-from .RzEmbedding import RzEmbedding
-from .ZzEmbedding import ZzEmbedding
-from .AmplitudeEmbedding import AmplitudeEmbedding
-from .DenseAngleEmbedding import DenseAngleEmbedding
-from .HigherOrderEmbedding import HigherOrderEmbedding
+from .RxEmbedding import RxEmbedding as _RxEmbedding
+from .RyEmbedding import RyEmbedding as _RyEmbedding
+from .RzEmbedding import RzEmbedding as _RzEmbedding
+from .ZzEmbedding import ZzEmbedding as _ZzEmbedding
+from .AmplitudeEmbedding import AmplitudeEmbedding as _AmplitudeEmbedding
+from .DenseAngleEmbedding import DenseAngleEmbedding as _DenseAngleEmbedding
+from .HigherOrderEmbedding import HigherOrderEmbedding as _HigherOrderEmbedding
 
-# Ansatzs
-from .HardwareEfficient import HardwareEfficient
-from .HCzRx import HCzRx
-from .TreeTensor import TreeTensor
-from .TwoLocal import TwoLocal
-from .Annular import Annular
+from .Ansatzs import __all__ as __ansatz__all__
+from .Embeddings import __all__ as __embedding__all__
 
-__all__ = ['RxEmbedding', 'RyEmbedding', 'RzEmbedding', 'ZzEmbedding', 'AmplitudeEmbedding', 'DenseAngleEmbedding', 'HigherOrderEmbedding', 'HardwareEfficient', 'HCzRx', 'TreeTensor', 'TwoLocal', 'Annular']
+__all__ = __ansatz__all__ + __embedding__all__ + ['_RxEmbedding', '_RyEmbedding', '_RzEmbedding', '_ZzEmbedding', '_AmplitudeEmbedding', '_HigherOrderEmbedding', '_DenseAngleEmbedding']
--- lazyqml/Factories/fCircuits.py
+++ lazyqml/Factories/fCircuits.py
@@ -1,7 +1,11 @@
 # Importing Enums
 from lazyqml.Global.globalEnums import Ansatzs, Embedding
 # Importing Circuits
-from lazyqml.Circuits import *
+from lazyqml.Circuits.Ansatzs import HardwareEfficient, HCzRx, TreeTensor, TwoLocal, Annular
+from lazyqml.Circuits.Embeddings import DenseAngleEmbedding, ZZEmbedding, HigherOrderEmbedding
+
+from functools import partial
+import pennylane as qml
 
 class CircuitFactory:
     def __init__(self, nqubits, nlayers) -> None:
@@ -22,16 +26,26 @@ class CircuitFactory:
 
     def GetEmbeddingCircuit(self, embedding):
         if embedding == Embedding.RX:
-            return RxEmbedding()
+            return partial(qml.AngleEmbedding, rotation='X')
+        
         elif embedding == Embedding.RY:
-            return RyEmbedding()
+            return partial(qml.AngleEmbedding, rotation='Y')
+        
         elif embedding == Embedding.RZ:
-            return RzEmbedding()
+            def RZ(inputs, wires):
+                [qml.Hadamard(i) for i in wires]
+                qml.AngleEmbedding(inputs, wires, rotation='Z')
+
+            return RZ
+        
         elif embedding == Embedding.ZZ:
-            return ZzEmbedding()
+            return ZZEmbedding
+        
         elif embedding == Embedding.AMP:
-            return AmplitudeEmbedding()
+            return partial(qml.AmplitudeEmbedding, pad_with=0, normalize=True)
+        
         elif embedding == Embedding.DENSE_ANGLE:
-            return DenseAngleEmbedding()
+            return DenseAngleEmbedding
+        
         elif embedding == Embedding.HIGHER_ORDER:
-            return HigherOrderEmbedding()
+            return HigherOrderEmbedding
