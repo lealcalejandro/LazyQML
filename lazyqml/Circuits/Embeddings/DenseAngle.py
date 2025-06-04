0f0f0cfcd90ce6450701a13f4f160ae466091ef1
lealcalejandro <lealcalejandro@uniovi.es>
2025-06-04 12:11:24 +0200

    Fixed error message in DenseAngle embedding

--- lazyqml/Circuits/Embeddings/DenseAngle.py
+++ lazyqml/Circuits/Embeddings/DenseAngle.py
@@ -13,7 +13,7 @@ class DenseAngleEmbedding(Operation):
         n_features = shape[0]
         if n_features > 2*len(wires):
             raise ValueError(
-                f"Features must be of length {len(wires)} or less; got length {n_features}."
+                f"Features must be of length {2*len(wires)} or less; got length {n_features}."
             )
 
         self._hyperparameters = {}
@@ -29,20 +29,11 @@ class DenseAngleEmbedding(Operation):
     @staticmethod
     def compute_decomposition(features, wires):
 
-        batched = qml.math.ndim(features) > 1
-
-        # padding del stateprep (usado en el amplitude embedding)
-        # if n_states < dim:
-        #     padding = [pad_with] * (dim - n_states)
-        #     if len(shape) > 1:
-        #         padding = [padding] * shape[0]
-        #     padding = math.convert_like(padding, state)
-        #     state = math.hstack([state, padding])
-
         op_list = []
 
-        n = len(wires)
+        n_qubits = len(wires)
 
+        batched = qml.math.ndim(features) > 1
         shape = tuple(features.shape)
         n_features = shape[0] if not batched else shape[1]
 
@@ -53,19 +44,18 @@ class DenseAngleEmbedding(Operation):
         #         features = F.pad(features, (0, 2*n - n_features), 'constant', 0)
 
         # print(shape, features)
-        if n_features < 2*n:
-            padding = [0] * (2*n - n_features)
+        if n_features < 2*n_qubits:
+            padding = [0] * (2*n_qubits - n_features)
             if len(shape) > 1:
                 padding = [padding] * shape[0]
             padding = qml.math.convert_like(padding, features)
             features = qml.math.hstack([features, padding])
-        # print(features)
 
         features = qml.math.T(features) if batched else features
 
         # qml.AngleEmbedding(x[..., :N], wires=wires, rotation='Y')
         for i in wires:
             op_list.append(qml.RY(features[i], wires=i))
-            op_list.append(qml.PhaseShift(features[n + i], wires=i))
+            op_list.append(qml.PhaseShift(features[n_qubits + i], wires=i))
 
         return op_list
