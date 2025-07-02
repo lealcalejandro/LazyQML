6431a094c02e5d9919658fdd13194217d5ffe570
lealcalejandro <lealcalejandro@uniovi.es>
2025-07-02 12:07:54 +0200

    Added missing Enum imports

--- lazyqml/GUI/_generator.py
+++ lazyqml/GUI/_generator.py
@@ -159,7 +159,8 @@ def generate_code(button):
     imports = "\n".join(
         [
             f"from sklearn.datasets import load_{selected_dataset}",
-            "from lazyqml import QuantumClassifier"
+            "from lazyqml import QuantumClassifier",
+            "from lazyqml.Global import *"
         ]
     )
 
