cd43aa9e5d2e0836d83218dfc0300578f8dbb77e
lealcalejandro <lealcalejandro@uniovi.es>
2025-07-23 10:32:09 +0200

    Removed relative imports from old embeddings

--- lazyqml/Circuits/__init__.py
+++ lazyqml/Circuits/__init__.py
@@ -1,14 +1,4 @@
-## Legacy
-# Embeddings
-from .RxEmbedding import RxEmbedding as _RxEmbedding
-from .RyEmbedding import RyEmbedding as _RyEmbedding
-from .RzEmbedding import RzEmbedding as _RzEmbedding
-from .ZzEmbedding import ZzEmbedding as _ZzEmbedding
-from .AmplitudeEmbedding import AmplitudeEmbedding as _AmplitudeEmbedding
-from .DenseAngleEmbedding import DenseAngleEmbedding as _DenseAngleEmbedding
-from .HigherOrderEmbedding import HigherOrderEmbedding as _HigherOrderEmbedding
-
 from .Ansatzs import __all__ as __ansatz__all__
 from .Embeddings import __all__ as __embedding__all__
 
-__all__ = __ansatz__all__ + __embedding__all__ + ['_RxEmbedding', '_RyEmbedding', '_RzEmbedding', '_ZzEmbedding', '_AmplitudeEmbedding', '_HigherOrderEmbedding', '_DenseAngleEmbedding']
+__all__ = __ansatz__all__ + __embedding__all__
