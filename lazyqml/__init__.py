756a2cb01181b1312bab1e03f2db39561c0a610d
lealcalejandro <lealcalejandro@uniovi.es>
2025-07-24 11:26:27 +0200

    Bump version: 0.0.8 → 0.0.9

--- lazyqml/__init__.py
+++ lazyqml/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Diego García Vega, Fernando Álvaro Plou Llorente, Alejandro Leal Castaño"""
 __email__ = "garciavdiego@uniovi.es, ploufernando@uniovi.es, lealcalejandro@uniovi.es"
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 from .lazyqml import QuantumClassifier
 
